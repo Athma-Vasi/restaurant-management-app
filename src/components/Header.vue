0ccded94ea84c3d71954e0cc4adef5f20d6ae396
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-02-22 17:56:34 -0700

    add header nav bar

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Add Restaurant</a>
+      <a href="#">Update Restaurant</a>
+      <a href="#">Logout</a>
+    </nav>
+  </header>
+</template>
+
+<script lang="js">
+export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: white;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  font-size: larger;
+  padding: 10px;
+}
+
+a:hover {
+  background-color: #757575;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+  <Header />
   <h1>Hello User, Welcome to Home Page</h1>
 </template>
 
 <script>
+import Header from "./Header.vue";
+
 export default {
   name: "Home",
-
+  components: {
+    Header,
+  },
   mounted() {
     let user = localStorage.getItem("user");
     if (!user) {
