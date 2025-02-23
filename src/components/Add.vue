919bb27ad528a05aef9dbddb8b842bf91ac9b3d3
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-02-23 15:48:16 -0700

    add Add form, api call

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,26 @@
 <template>
   <Header />
   <h1>Hello {{ name }}, Welcome to Add Restaurant Page</h1>
+
+  <form action="post">
+    <fieldset class="form">
+      <legend>Add Restaurant</legend>
+
+      <label for="add-name"></label>
+      <input type="text" name="add-name" id="add-name" placeholder="Name" v-model="restaurant.name" />
+
+      <label for="add-location"></label>
+      <input type="text" name="add-location" id="add-location" placeholder="Location" v-model="restaurant.location" />
+
+      <label for="add-phone"></label>
+      <input type="text" name="add-phone" id="add-phone" placeholder="Phone" v-model="restaurant.phone" />
+
+      <label for="add-website"></label>
+      <input type="text" name="add-website" id="add-website" placeholder="Website" v-model="restaurant.website" />
+
+      <button type="button" v-on:click="addRestaurant">Add Restaurant</button>
+    </fieldset>
+  </form>
 </template>
 
 <script>
@@ -8,14 +28,53 @@ import Header from "./Header.vue";
 
 export default {
   name: "Add",
-  data() {
-    return {
-      name: "",
-    };
-  },
   components: {
     Header,
   },
+
+  data() {
+    return {
+      name: "",
+      restaurant: {
+        name: "",
+        location: "",
+        phone: "",
+        website: "",
+      }
+    };
+  },
+
+  components: {
+    Header,
+  },
+
+  methods: {
+    async addRestaurant() {
+      try {
+        console.group("Add Restaurant");
+        console.log("Name: ", this.restaurant.name);
+        console.log("Location: ", this.restaurant.location);
+        console.log("Phone: ", this.restaurant.phone);
+        console.log("Website: ", this.restaurant.website);
+        console.groupEnd();
+
+        let result = await axios.post("http://localhost:3000/restaurants", this.restaurant);
+        console.log("Result: ", result);
+
+        if (result.status === 201) {
+          alert("Restaurant added successfully");
+          this.$router.push({ name: "Home" });
+        } else {
+          alert("Failed to add restaurant");
+        }
+
+      } catch (error) {
+        console.error(error);
+
+      }
+    }
+  },
+
   mounted() {
     let user = localStorage.getItem("user");
     this.name = JSON.parse(user).name;
